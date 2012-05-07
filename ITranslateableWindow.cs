0c9405b456d73bddc1971caf11859827683add78
Matthew Barbour <sanmadjack@masgau.org>
2012-05-07 13:12:14 -0500

    Renamed show* methods to display* to help reveal untranslated strings

--- ITranslateableWindow.cs
+++ ITranslateableWindow.cs
@@ -8,10 +8,10 @@ namespace Translator
     public interface ITranslateableWindow
     {
         void setTranslatedTitle(string name, params string[] variables);
-        bool askQuestion(string title, string message);
-        bool showError(string title, string message);
-        bool showError(string title, string message, Exception exception);
-        bool showWarning(string title, string message);
-        bool showInfo(string title, string message);
+        bool displayQuestion(string title, string message);
+        bool displayError(string title, string message);
+        bool displayError(string title, string message, Exception exception);
+        bool displayWarning(string title, string message);
+        bool displayInfo(string title, string message);
     }
 }

e571839d0888edd6302af16d83c248b4ca957635
Matthew Barbour <sanmadjack@gmail.com>
2012-06-05 13:01:58 -0500

    Added special exception for when string is not found

---
+++ StringNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Translator {
+    public class StringNotFoundException: Exception {
+        public StringType type { get; protected set; }
+        public StringNotFoundException(string name, StringType type)
+            : base(name) {
+                this.type = type;
+        }
+        public StringNotFoundException(string name)
+            : base(name) {
+        }
+    }
+}
